30e5245d48ec05dbb45af16980247664ab31d9de
ramu siripalli <ramsiripalli2772@gmail.com>
2026-01-08 19:11:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+let submit = document.getElementById("submit");
+submit.addEventListener("click" , removeColor);
+
+function removeColor() {
+  var selectBox = document.getElementById("colorSelect");
+  var selectedIndex = selectBox.selectedIndex;
+
+  if (selectedIndex !== -1) {
+    selectBox.remove(selectedIndex);
+  }
+}
